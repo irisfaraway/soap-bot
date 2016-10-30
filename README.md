f410424ee027545fcb009051b561a3b88ee7bf4a
irisfaraway <iris.faraway@gmail.com>
2016-10-30 16:21:58 +0000

    eldritchify statements before tweeting

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # soap-bot
 
+The bot is currently up and running at [@eldritchenders](https://twitter.com/eldritchenders).
+
 scraper.rb collects plot summaries for individual Eastenders episodes from the BBC website.
 
-bots.rb uses them as fodder for tweets.
+bots.rb uses these summaries to compose tweets, but replaces some of the character names with Lovecraft stuff for added ELDRITCH HORROR.
+
+I made some tweaks to the scraper output to avoid some weirdness with full character names ("Cthulhu Beale").
--- bots.rb
+++ bots.rb
@@ -10,22 +10,49 @@ class MyBot < Ebooks::Bot
   def configure
     # Consumer details come from registering an app at https://dev.twitter.com/
     # Once you have consumer details, use "ebooks auth" for new access tokens
-    self.consumer_key = ENV['SOAP_BOT_CONSUMER_KEY'] # Your app consumer key
-    self.consumer_secret = ENV['SOAP_BOT_CONSUMER_SECRET'] # Your app consumer secret
-
-    # Users to block instead of interacting with
-    # self.blacklist = ['username']
-
-    # Range in seconds to randomize delay when bot.delay is called
-    self.delay_range = 1..6
+    self.consumer_key = ENV['SOAP_BOT_CONSUMER_KEY']
+    self.consumer_secret = ENV['SOAP_BOT_CONSUMER_SECRET']
   end
 
   def on_startup
     load_model!
 
-    scheduler.every '1m' do
-      statement = model.make_statement(140)
-      tweet(statement)
+    # Get the names of characters which can be replaced
+    @characters = File.foreach('characters.txt').map { |line| line.chomp }
+
+    # Get the eldritch words
+    @eldritch = File.foreach('eldritch.txt').map { |line| line.split("\n") }
+
+    scheduler.every '5m' do
+      # Kept the character count down to leave room for long Lovecraft names
+      statement = model.make_statement(100)
+
+      words = statement.split(' ')
+      @new_words = []
+
+      # Replace some capitalised words with eldritch horror
+      words.each_with_index do |word|
+        # Only replace words half of the time
+        @new_words << if rand > 0.5
+                        word
+                      else
+                        # Separate punctuation and anything after it, like 's
+                        word_bits = word.split(/(?<=\w)(?=[.,?':;])/)
+                        # Check if word is on character list
+                        if @characters.include?(word_bits[0])
+                          # Replace the word with ELDRITCH HORROR
+                          word_bits[0] = @eldritch.sample
+                        end
+                        # Re-add the punctuation
+                        word_bits.join
+                      end
+      end
+
+      # Create the new statement
+      new_statement = @new_words.join(' ')
+
+      # puts new_statement
+      tweet(new_statement)
     end
   end
 
@@ -42,7 +69,7 @@ class MyBot < Ebooks::Bot
 end
 
 # Make a MyBot and attach it to an account
-MyBot.new('wikisoapbot') do |bot|
-  bot.access_token = ENV['SOAP_BOT_TOKEN'] # Token connecting the app to this account
-  bot.access_token_secret = ENV['SOAP_BOT_TOKEN_SECRET'] # Secret connecting the app to this account
+MyBot.new('eldritchenders') do |bot|
+  bot.access_token = ENV['SOAP_BOT_TOKEN']
+  bot.access_token_secret = ENV['SOAP_BOT_TOKEN_SECRET']
 end
